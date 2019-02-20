518bcb4a90bd00859b745735a27d95ba83e8e447
Emidio Morgia <emidio@morgia.info>
2019-02-20 19:02:42 +0100

    Created Gateway Service, Discovery Service and Core Service

---
+++ coreservice/src/test/java/info/morgia/coreservice/CoreserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package info.morgia.coreservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
