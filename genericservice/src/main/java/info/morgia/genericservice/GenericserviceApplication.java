eedb22ce7e618a0454877e1ef9536a0f2d94d96f
Emidio Morgia <emidio@morgia.info>
2019-03-21 23:08:58 +0100

    Created Generic Microservice

---
+++ genericservice/src/main/java/info/morgia/genericservice/GenericserviceApplication.java
@@ -0,0 +1,15 @@
+package info.morgia.genericservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GenericserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericserviceApplication.class, args);
+	}
+
+}
---
+++ genericservice/src/main/java/info/morgia/genericservice/interfaces/login/web/LoginController.java
@@ -0,0 +1,18 @@
+package info.morgia.genericservice.interfaces.login.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping("/login")
+    public String Login(@RequestParam() String username,@RequestParam() String password){
+        return "OK";
+    }
+}
