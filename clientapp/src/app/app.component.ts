a6f729c27b09c6f6e89b11f36f794a6b7a7dd383
Emidio Morgia <emidio@morgia.info>
2019-05-01 11:51:43 +0200

    added ngbootstrap closes #2

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  
-  
+export class AppComponent implements OnInit {
   title = 'clientapp';
+
+  ngOnInit(): void {
+    this.jqBsInit();
+  }
+
+  private jqBsInit() {
+    var treeviewMenu = $('.app-menu');
+    // Toggle Sidebar
+    $('[data-toggle="sidebar"]').click(function (event) {
+      event.preventDefault();
+      $('.app').toggleClass('sidenav-toggled');
+    });
+    // Activate sidebar treeview toggle
+    $("[data-toggle='treeview']").click(function (event) {
+      event.preventDefault();
+      if (!$(this).parent().hasClass('is-expanded')) {
+        treeviewMenu.find("[data-toggle='treeview']").parent().removeClass('is-expanded');
+      }
+      $(this).parent().toggleClass('is-expanded');
+    });
+    // Set initial active toggle
+    $("[data-toggle='treeview.'].is-expanded").parent().toggleClass('is-expanded');
+  }
 }
