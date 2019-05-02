e3842f5b17101796b31120e63531963f88bb3d87
Emidio Morgia <emidio@morgia.info>
2019-05-02 22:21:01 +0200

    added layouts

--- clientapp/src/app/app.module.ts
+++ clientapp/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
+import { AppLayoutComponent } from './shared/app-layout/app-layout/app-layout.component';
+import { LoginLayoutComponent } from './shared/login-layout/login-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AppLayoutComponent,
+    LoginLayoutComponent
   ],
   imports: [
     BrowserModule,
