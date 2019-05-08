98d910fcc892aee2f613812ed069e754dedd71d5
Emidio Morgia <emidio@morgia.info>
2019-05-08 19:14:55 +0200

    separated layout anc mocked login

--- clientapp/src/app/infrastructure/auth.service.ts
+++ clientapp/src/app/infrastructure/auth.service.ts
@@ -8,25 +8,20 @@ export class AuthService {
   constructor() { }
 
   public isAuthenticated(): boolean {
-    debugger;
     const token: string = this.getAuthToken();
     return token != null;
-
   }
 
   public getAuthToken(): string {
-    debugger;
     return localStorage.getItem('auth_token');
   }
 
   public setAuthToken(token: string) {
-    debugger;
     if (token != null) {
       localStorage.setItem('auth_token', token);
     } else {
       localStorage.removeItem('auth_token');
     }
-
   }
 
 }
--- clientapp/src/app/infrastructure/guard.service.ts
+++ clientapp/src/app/infrastructure/guard.service.ts
@@ -9,20 +9,17 @@ export class GuardService implements CanActivate {
   authService: AuthService;
   router: Router;
 
-  constructor(authService: AuthService, router: Router){
+  constructor(authService: AuthService, router: Router) {
     this.authService = authService;
     this.router = router;
   }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    debugger;
     const isAuthenticated: boolean = this.authService.isAuthenticated();
     if (!isAuthenticated) {
       this.router.navigate(['login']);
-      console.log('canActivate=false');
       return false;
     }
-    console.log('canActivate=true');
     return true;
   }
 }
