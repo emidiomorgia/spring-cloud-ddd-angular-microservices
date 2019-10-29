768362248af0fc97448761f9e63b36faef3c21d9
emidiomorgia <emidio@morgia.info>
2019-10-29 22:44:08 +0100

    discovery service

--- discoveryservice/src/main/java/info/morgia/akron/discoveryservice/DiscoveryserviceApplication.java
+++ discoveryservice/src/main/java/info/morgia/akron/discoveryservice/DiscoveryserviceApplication.java
@@ -2,8 +2,10 @@ package info.morgia.akron.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryserviceApplication {
 
 	public static void main(String[] args) {
