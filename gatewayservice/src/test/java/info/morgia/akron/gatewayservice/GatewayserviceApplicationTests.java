c5ab923f2e9ff5aae356216057ac032bc8b94720
emidiomorgia <emidio@morgia.info>
2019-10-30 20:14:37 +0100

    gateway

---
+++ gatewayservice/src/test/java/info/morgia/akron/gatewayservice/GatewayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package info.morgia.akron.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- identityservice/src/main/java/info/morgia/akron/identityservice/HomeController.java
+++ identityservice/src/main/java/info/morgia/akron/identityservice/HomeController.java
@@ -1,5 +1,6 @@
 package info.morgia.akron.identityservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/home")
 public class HomeController {
 
+    
+    private String serviceValue="ok";
+
     @GetMapping(path = "/value", produces= MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HomeResponse> getValue(){
-        return new ResponseEntity<HomeResponse>(new HomeResponse(1,"emidio"),HttpStatus.OK );
+        return new ResponseEntity<HomeResponse>(new HomeResponse(1,serviceValue),HttpStatus.OK );
     }
 }
