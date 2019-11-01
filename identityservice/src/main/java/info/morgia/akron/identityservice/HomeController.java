e399b117faed4f1cda2164ad9c2f84247dd05e73
emidiomorgia <emidio@morgia.info>
2019-11-01 11:42:53 +0100

    read property

--- identityservice/src/main/java/info/morgia/akron/identityservice/HomeController.java
+++ identityservice/src/main/java/info/morgia/akron/identityservice/HomeController.java
@@ -12,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/home")
 public class HomeController {
 
-    
-    private String serviceValue="ok";
+    @Value("${server.port:v1}")
+    private String v;
+
+    @Value("${sapp.value:v2}")
+    private String v2;
 
     @GetMapping(path = "/value", produces= MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<HomeResponse> getValue(){
-        return new ResponseEntity<HomeResponse>(new HomeResponse(1,serviceValue),HttpStatus.OK );
+        return new ResponseEntity<HomeResponse>(new HomeResponse(1,v),HttpStatus.OK );
     }
 }
