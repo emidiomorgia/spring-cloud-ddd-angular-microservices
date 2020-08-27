c88eb294732fb06da7d31b685c77f8f22f0fa445
emidio@morgia.info <emidio@morgia.info>
2020-08-27 11:30:09 +0200

    work in progress

--- counters-service/src/main/java/info/morgia/counters/countersservice/interfaces/web/home/HomeController.java
+++ counters-service/src/main/java/info/morgia/counters/countersservice/interfaces/web/home/HomeController.java
@@ -1,7 +1,8 @@
 package info.morgia.counters.countersservice.interfaces.web.home;
 
-import java.util.Date;
-
+import java.net.InetAddress;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/home")
 public class HomeController {
-    
+    @Autowired
+    Environment environment;
+
     @GetMapping("/getValue")
     public String getValue() {
-        return new Date().toString();
+        String res="ERROR";
+        try {
+            // Port
+            String port= environment.getProperty("server.port");
+            // Local address
+            String localAddress=InetAddress.getLocalHost().getHostAddress();
+            localAddress += " " + InetAddress.getLocalHost().getHostName();
+
+            // Remote address
+            String remoteAddress = InetAddress.getLoopbackAddress().getHostAddress();
+            remoteAddress += " " + InetAddress.getLoopbackAddress().getHostName();
+
+            res = "ADDR: " + localAddress +", " + remoteAddress + ": " + port;
+        } catch (Exception e) {
+            
+            res="ERROR";
+        }
+        
+
+        return res;
     }
 }
