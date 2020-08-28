e8d444c03102e3b9f4b0b2ec9fb38701e83fdf4a
emidio@morgia.info <emidio@morgia.info>
2020-08-28 18:27:20 +0200

    Project structure and docker-compose

---
+++ ui-service/server/src/test/java/info/morgia/counters/uiservice/UiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package info.morgia.counters.uiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
